425ec95e34ae9b598749d56bee0bb1e6cf080933
Landon Gingerich <landon.ging@gmail.com>
2026-03-04 20:45:25 -0600

    add capaciry check and debug asserts

--- ring-buffer/src/lib.rs
+++ ring-buffer/src/lib.rs
@@ -8,16 +8,23 @@ pub struct RingBuffer<T> {
 
 
 impl<T> RingBuffer<T> {
-    pub fn new(capacity: usize) -> Self {
-        Self {
+    pub fn new(capacity: usize) -> Result<Self, &'static str> {
+        if capacity == 0 {
+            return Err("capacity must be > 0")
+        }
+
+        Ok(Self {
             buf: (0..capacity).map(|_| None).collect(),
             head: 0,
             tail: 0,
             size: 0,
-        }
+        })
     }
 
     pub fn push(&mut self, val: T) -> Result<(), T> {
+        debug_assert!(self.tail < self.buf.len(), "tail out of bounds" );
+        debug_assert!(self.size <= self.buf.len(), "size exceeds capacity");
+
         if self.size == self.buf.len() {
             return Err(val); // error when ring buffer is full
         }
@@ -25,18 +32,27 @@ impl<T> RingBuffer<T> {
         self.buf[self.tail] = Some(val);
         self.tail = (self.tail + 1) % self.buf.len();
         self.size += 1;
-        // assert!(head != tail)
+        
+        debug_assert!(self.tail < self.buf.len(), "tail out of bounds" );
+        debug_assert!(self.size <= self.buf.len(), "size exceeds capacity");
 
         Ok(())
     }
 
     pub fn pop(&mut self) -> Option<T> {
+        debug_assert!(self.head < self.buf.len(), "head out of bounds" );
+        debug_assert!(self.size <= self.buf.len(), "size exceeds capacity");
+        
         if self.size == 0 {
             return None; // empty, nothing to pop
         }
         let out = self.buf[self.head].take();
         self.head = (self.head + 1) % self.buf.len();
         self.size -= 1;
+
+        debug_assert!(self.head < self.buf.len(), "head out of bounds" );
+        debug_assert!(self.size <= self.buf.len(), "size exceeds capacity");
+
         out
     }
 }
@@ -44,8 +60,6 @@ impl<T> RingBuffer<T> {
 
 
 
-// insert (push) at tail
-// extract (pop) at head
 
 // assert!(head < size)
 // assert!(tail < size)
